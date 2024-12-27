c5ae91a86dbf92f3af9ee7d9598be73fee3bf9e6
vktadm <tarnavskayavictoria@gmail.com>
2024-12-27 17:11:04 +0300

    End bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
@@ -16,10 +17,16 @@ async def info(message: types.Message):
         [types.KeyboardButton(text='Open web page', web_app=WebAppInfo(url='https://vktadm.github.io/tgWebApp/'))],
     ]
     # Inline кнопки в чате
-    markup = types.ReplyKeyboardMarkup(inline_keyboard=button_list)
+    markup = types.ReplyKeyboardMarkup(keyboard=button_list)
     await message.reply('Hello 🦁', reply_markup=markup)
 async def main():
     await dp.start_polling(bot)
+
+@dp.message(F.content_type == types.ContentType.WEB_APP_DATA)
+async def get_data(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'The order has been placed ✅\nName: {res["name"]}.'
+                         f'\ne-mail: {res["email"]}.\nphone: {res["phone"]}')
 
 if __name__ == "__main__":
     asyncio.run(main())
